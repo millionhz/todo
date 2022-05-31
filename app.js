6e8131443fafb8fe4d4acbe6ae87959f7565e4e9
millionhz <m.waqas.hamza@gmail.com>
2022-05-31 10:37:49 +0500

    implement rearrange

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ function _getListGroupItem(todoItemText, todoItemIdx) {
     "list-group-item d-flex justify-content-between align-items-center"
   );
   todoItem.setAttribute("data-idx", todoItemIdx);
+  todoItem.setAttribute("draggable", "true");
 
   todoItem.appendChild(textElement);
   todoItem.appendChild(buttonElement);
@@ -69,6 +70,17 @@ function removeTodoListItem(idxToRemove) {
   rebuildTodoList();
 }
 
+function rearrangeTodoListItems(from, to) {
+  const todoListArr = getTodoList();
+
+  const todoListItem = todoListArr.splice(from, 1);
+  todoListArr.splice(to, 0, todoListItem);
+
+  setTodoList(todoListArr);
+
+  rebuildTodoList();
+}
+
 function _manageTodoInputEvent() {
   const todoItem = inputField.value;
   appendTodoListItem(todoItem);
@@ -90,5 +102,33 @@ inputField.addEventListener("keypress", (event) => {
 todoList.addEventListener("click", (event) => {
   if (event.target.nodeName === "BUTTON") {
     removeTodoListItem(event.target.parentElement.dataset.idx);
+  }
+});
+
+todoList.addEventListener("dragstart", (event) => {
+  if (event.target.nodeName === "UL") {
+    event.dataTransfer.setData("text/plain", event.target.dataset.idx);
+  }
+});
+
+todoList.addEventListener("dragover", (event) => {
+  event.preventDefault();
+  event.dataTransfer.dropEffect = "move";
+});
+
+todoList.addEventListener("drop", (event) => {
+  event.preventDefault();
+
+  const from = event.dataTransfer.getData("text/plain");
+  let to = null;
+
+  if (event.target.nodeName === "UL") {
+    to = event.target.dataset.idx;
+  } else {
+    to = event.target.parentElement.dataset.idx;
+  }
+
+  if (from !== to) {
+    rearrangeTodoListItems(from, to);
   }
 });
