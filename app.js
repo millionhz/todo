a163a54b52f2166e9457db0e9cd6bbd19f6db03c
Muhammad Hamza <m.waqas.hamza@gmail.com>
2023-01-14 17:40:16 +0500

    rewrite to directly add event to listitems

--- app.js
+++ app.js
@@ -14,6 +14,47 @@ function setTodoList(todoItems) {
   localStorage.setItem('todoList', JSON.stringify(todoItems));
 }
 
+function handleDelete(event) {
+  removeTodoListItem(event.target.parentElement.dataset.idx);
+}
+
+function handleDragStart(event) {
+  event.dataTransfer.setData('text/plain', event.target.dataset.idx);
+  event.target.classList.add('list-group-item-primary');
+}
+
+function handleDragEnd(event) {
+  event.target.classList.remove('active');
+  event.target.classList.remove('list-group-item-primary');
+}
+
+function handleDragOver(event) {
+  event.preventDefault();
+  event.dataTransfer.dropEffect = 'move';
+
+  event.target.classList.add('active');
+}
+
+function handleDragLeave(event) {
+  event.target.classList.remove('active');
+}
+
+function handleDrop(event) {
+  event.preventDefault();
+
+  const from = event.dataTransfer.getData('text/plain');
+
+  if (event.target.nodeName === 'UL') {
+    to = event.target.dataset.idx;
+  } else {
+    to = event.target.parentElement.dataset.idx;
+  }
+
+  if (from !== to) {
+    rearrangeTodoListItems(from, to);
+  }
+}
+
 function _getListGroupItem(todoItemText, todoItemIdx) {
   const textElement = document.createElement('p');
   textElement.setAttribute('class', 'mx-0 my-0 text-start text-break pe-2');
@@ -22,7 +63,7 @@ function _getListGroupItem(todoItemText, todoItemIdx) {
   const buttonElement = document.createElement('button');
   buttonElement.setAttribute('class', 'btn btn-outline-danger btn-sm');
   buttonElement.setAttribute('type', 'button');
-  buttonElement.setAttribute('title', 'delete-todo');
+  buttonElement.addEventListener('click', handleDelete);
   buttonElement.textContent = 'Delete';
 
   const todoItem = document.createElement('ul');
@@ -35,6 +76,12 @@ function _getListGroupItem(todoItemText, todoItemIdx) {
 
   todoItem.appendChild(textElement);
   todoItem.appendChild(buttonElement);
+
+  todoItem.addEventListener('dragstart', handleDragStart);
+  todoItem.addEventListener('dragend', handleDragEnd);
+  todoItem.addEventListener('dragover', handleDragOver);
+  todoItem.addEventListener('dragleave', handleDragLeave);
+  todoItem.addEventListener('drop', handleDrop);
 
   return todoItem;
 }
@@ -89,63 +136,9 @@ function _manageTodoInputEvent() {
 
 window.onfocus = rebuildTodoList;
 
-document.addEventListener('DOMContentLoaded', () => {
-  rebuildTodoList();
-});
+document.addEventListener('DOMContentLoaded', rebuildTodoList);
 
 inputForm.addEventListener('submit', (event) => {
   event.preventDefault();
   _manageTodoInputEvent();
-});
-
-todoList.addEventListener('click', (event) => {
-  if (event.target.title === 'delete-todo') {
-    removeTodoListItem(event.target.parentElement.dataset.idx);
-  }
-});
-
-todoList.addEventListener('dragstart', (event) => {
-  if (event.target.nodeName === 'UL') {
-    event.dataTransfer.setData('text/plain', event.target.dataset.idx);
-    event.target.classList.add('list-group-item-primary');
-  }
-});
-
-todoList.addEventListener('dragend', (event) => {
-  if (event.target.nodeName === 'UL') {
-    event.target.classList.remove('active');
-    event.target.classList.remove('list-group-item-primary');
-  }
-});
-
-todoList.addEventListener('dragover', (event) => {
-  event.preventDefault();
-  event.dataTransfer.dropEffect = 'move';
-
-  if (event.target.nodeName === 'UL') {
-    event.target.classList.add('active');
-  }
-});
-
-todoList.addEventListener('dragleave', (event) => {
-  if (event.target.nodeName === 'UL') {
-    event.target.classList.remove('active');
-  }
-});
-
-todoList.addEventListener('drop', (event) => {
-  event.preventDefault();
-
-  const from = event.dataTransfer.getData('text/plain');
-  let to = null;
-
-  if (event.target.nodeName === 'UL') {
-    to = event.target.dataset.idx;
-  } else {
-    to = event.target.parentElement.dataset.idx;
-  }
-
-  if (from !== to) {
-    rearrangeTodoListItems(from, to);
-  }
 });
